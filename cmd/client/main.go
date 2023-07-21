32f84ec63a6125c7ab2196bf75b756e5afb454d7
hrisancho <catminkyn822@gmail.com>
2023-07-21 18:38:02 +0300

    ini

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"log"
+	"time"
 
 	"GSS/internal/client"
 	"GSS/internal/client/config"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
 )
 
 func main() {
@@ -22,4 +36,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Код который написн ниже считается говно-кодом)))
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.Hi(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"GSS/proto/grpc"
+	"context"
+	"flag"
+	"fmt"
 	"log"
+	"net"
 
-	"GSS/internal/server"
 	"GSS/internal/server/config"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) Hi(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hi,  " + in.GetName()}, nil
+}
 
 func main() {
 	cfg, err := config.LoadConfig("./config/server/")
@@ -13,7 +30,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	server := server.NewServer(cfg)
+	#server := server.NewServer(cfg)
 
-	server.Run()
+	#server.Run()
+
+	// Код который написн ниже считается говно-кодом)))
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
