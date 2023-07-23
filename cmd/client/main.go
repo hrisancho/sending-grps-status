629f866ff6c0a79971bdf09444fef5b77cbcfd60
hrisancho <catminkyn822@gmail.com>
2023-07-23 20:14:15 +0300

    hi is live !!!

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,35 +1,33 @@
 package main
 
 import (
-	"GSS/internal/client"
-	"GSS/internal/client/config"
 	"GSS/internal/metrics"
 	"context"
 	"time"
 
+	pb "GSS/proto/grpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"log"
 )
 
 var addr = "localhost:50051"
 
 func main() {
-	cfg, err := config.LoadConfig("./config/client")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	user, err := client.NewUser(cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = user.StreamingMetrics()
-	if err != nil {
-		log.Fatal(err)
-	}
+	//cfg, err := config.LoadConfig("./config/client")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//user, err := client.NewUser(cfg)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//err = user.StreamingMetrics()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 	// Код который написн ниже считается говно-кодом)))
 	mStorage, err := metrics.Get()
 	if err != nil {
@@ -41,12 +39,12 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
+	c := pb.NewStatusClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	//ссылки
-	meseg = &pb.StateRequest{
+	meseg := &pb.StateRequest{
 		NextGC:      mStorage.MetricMapUint32["NextGC"],
 		NumForcedGC: mStorage.MetricMapUint32["NumForcedGC"],
 
@@ -78,11 +76,11 @@ func main() {
 		TotalMemory:  mStorage.MetricMapUint64["TotalMemory"],
 		FreeMemory:   mStorage.MetricMapUint64["FreeMemory"],
 
-		GCCPUFraction: mStorage.MetricMapFloat64["GCCPUFraction"],
+		GCCPUFraction: float32(mStorage.MetricMapFloat64["GCCPUFraction"]),
 	}
 	r, err := c.StateFun(ctx, meseg)
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
+	log.Printf("Greeting: ", r)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,43 +1,40 @@
 package main
 
 import (
-	"GSS/internal/server"
-	"GSS/internal/server/config"
+	pb "GSS/proto/grpc"
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"log"
 	"net"
 )
 
 // TODO сделать методы для вызова их через grpc
 type GrpcServer struct {
-	pb.UnimplementedGreeterServer
+	pb.UnimplementedStatusServer
 }
 
 func (s *GrpcServer) StateFun(ctx context.Context, in *pb.StateRequest) (*pb.StateReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-	//TODO понять как работает return в grpc
+	log.Printf("Received: ", in)
+	return &pb.StateReply{Hi: "Hello "}, nil
 }
 
 func main() {
-	cfg, err := config.LoadConfig("./config/server/")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	server := server.NewServer(cfg)
-
-	server.Run()
+	//cfg, err := config.LoadConfig("./config/server/")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//server := server.NewServer(cfg)
+	//
+	//server.Run()
 	// Код который написн ниже считается говно-кодом)))
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50051))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &GrpcServer{})
+	pb.RegisterStatusServer(s, &GrpcServer{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
